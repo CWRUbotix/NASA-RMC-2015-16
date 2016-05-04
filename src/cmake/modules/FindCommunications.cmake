f77b479aac5d7b1c2b51ce86de7e9448b575a9d9
Andrew Wood <aew61@case.edu>
2016-05-03 22:32:00 -0400

    finished renaming Networking -> Communications

--- src/cmake/modules/FindCommunications.cmake
+++ src/cmake/modules/FindCommunications.cmake
@@ -1,4 +1,4 @@
-find_path( NETWORKING_INCLUDES Networking/CommTransmitter.hpp
+find_path( COMMUNICATIONS_INCLUDES Communications/CommTransmitter.hpp
     HINTS
         ${CMAKE_PREFIX_PATH}/include
 )
@@ -6,24 +6,24 @@ find_path( NETWORKING_INCLUDES Networking/CommTransmitter.hpp
 # find libraries depending on OS.
 if( ${CMAKE_SYSTEM_NAME} MATCHES "Windows" )
     # handle dll AND lib?
-    find_path( NETWORKING_SHARED_LIB Networking.dll
+    find_path( COMMUNICATIONS_SHARED_LIB Networking.dll
         HINTS
             ${CMAKE_PREFIX_PATH}/bin
     )
-    find_library( NETWORKING_LIB Networking
+    find_library( COMMUNICATIONS_LIB Networking
         HINTS
             ${CMAKE_PREFIX_PATH}/lib
     )
 else()
-    find_library( NETWORKING_SHARED_LIB Networking
+    find_library( COMMUNICATIONS_SHARED_LIB Networking
         HINTS
             ${CMAKE_PREFIX_PATH}/lib
     )
 endif()
 
-if( NOT NETWORKING_INCLUDES OR NOT NETWORKING_SHARED_LIB )
-    set( NETWORKING_FOUND FALSE )
+if( NOT COMMUNICATIONS_INCLUDES OR NOT COMMUNICATIONS_SHARED_LIB )
+    set( COMMUNICATIONS_FOUND FALSE )
 else()
     # set( ASYNC_SHARED_LIB ${ASYNC_SHARED_LIB}/Async.dll )
-    set( NETWORKING_FOUND TRUE )
+    set( COMMUNICATIONS_FOUND TRUE )
 endif()
