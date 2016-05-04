4ca72cb4f69af037b6df99e9ea63df9722bc3c4a
Andrew Wood <aew61@case.edu>
2016-05-03 22:09:14 -0400

    the main executable for a Robos build with Networking!

---
+++ src/Main/main.cpp
@@ -0,0 +1,36 @@
+// SYSTEM INCLUDES
+#include <Robos/Robos.hpp>
+#include <Networking/CommTransmitter.hpp>
+#include <Networking/CommReceiver.hpp>
+#include <Utilities/OSUtils.hpp>
+#include <chrono>
+#include <thread>
+
+
+// C++ PROJECT INCLUDES
+
+
+int main(int argc, char** argv)
+{
+    // initialize Nodes
+    auto pNetworkingNode = std::make_shared<Network::CommReceiver>();
+
+    // initialize Robos
+    Robos::Init(Utilities::OS::GetCurrentDirectory() + Utilities::OS::GetPathDelimiter() + "AsyncConfig.xml");
+
+    // register Nodes with Robos
+    Robos::Register(pNetworkingNode);
+
+    // Start Robos
+    Robos::Start();
+
+    // wait for timeout (or for a Node to kill)
+    std::this_thread::sleep_for(std::chrono::seconds(10));
+    // in the real version...call Robos::WaitForShutdown();
+    // this thread will go to sleep until a Node makes a call to ShutdownRobos();
+    // and then this thread will be woken up.
+
+    // stop Robos
+    Robos::Stop();
+    return 0;
+}
