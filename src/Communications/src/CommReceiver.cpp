a1f8376dffc859874bd720509794de61b22de3b5
Andrew Wood <aew61@case.edu>
2016-05-03 20:59:51 -0400

    small fix

--- src/Communications/src/CommReceiver.cpp
+++ src/Communications/src/CommReceiver.cpp
@@ -1,6 +1,6 @@
 #include <Robos/Robos.hpp>
 
-#include "CommReciever.hpp"
+#include "CommReceiver.hpp"
 #include "Network.hpp"
 #include "Messages/MotorControlMessage.hpp"
 #include "client.h"
@@ -18,21 +18,18 @@ namespace Network
 	void CommReciever::MainCallbackImpl()
 	{
 		//initialize connection here
-	        initialize_client(5005, 100, "192.168.0.1"); //IP address of control station, subject to change
+        // initialize_client();
 		
 		while(Robos::IsRunning())
 		{
 			//implement client code here
 			//wait to recieve instruction and then send message to appropriate channel
-			char* command = get_command();
-			printf("%s", command);
-
-
-			// auto pMessage = std::make_shared<ManualControl>();
+            // auto pMessage = std::make_shared<ManualControl>();
             
-		        // parse out data from packets, set fields of message
+            // parse out data from packets, set fields of message
             
-			// this->PublishMessage(pMessage);
+
+            // this->PublishMessage(pMessage);
 		}
 	}
 }
