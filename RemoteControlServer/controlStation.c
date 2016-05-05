44c6a8fbbbfacccd39e799d9886ad0eb81ceb0ab
Paul Nettleton <paulnett7@hotmail.com>
2016-05-05 16:49:18 -0400

    basis for sending a message to manually control motors

--- RemoteControlServer/controlStation.c
+++ RemoteControlServer/controlStation.c
@@ -8,10 +8,19 @@ int main (int argc, char** argv)
 {
 	if (argc != 2)
 	{
-		printf ("Error: please enter a command\n");
+		printf ("Error: please enter an IP address.\n");
 		exit (-1);
 	}
-	initialize_server (5005, 100, "192.168.0.2"); //IP address for the robot, subject to change
-	send_command (argv, strlen(argv) +1); //testing only, will eventually being setting up a continuous loop
+
+	initialize_server (5005, 100, argv[0]); //open socket at port 5005 at given IP with timeout of 100
+
+	char command[MAX_RECV_LEN]; //change the length of the input command to max length of a command
+	while (command[0] != 'q')
+	{
+		scanf("%s", command);
+		send_command (command, strlen(command) +1); //send the command
+		printf ("%s", get_receipt()); //get response
+	}
 	cleanup_server();
+	exit(1);
 }
