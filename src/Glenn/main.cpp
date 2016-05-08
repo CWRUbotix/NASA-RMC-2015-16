cd1ec85e9ec266cfa7bd1c8dff66a830d6c2e128
Anno van den Akker <annovda@gmail.com>
2016-05-08 16:45:07 -0400

    find router ip, find computers connected to router

--- src/Glenn/main.cpp
+++ src/Glenn/main.cpp
@@ -6,6 +6,8 @@
 #include <Utilities/OSUtils.hpp>
 #include <chrono>
 #include <thread>
+#include <stdio.h>
+#include <string.h>
 
 
 // C++ PROJECT INCLUDES
@@ -13,28 +15,72 @@
 
 int main(int argc, char** argv)
 {
-    // initialize Nodes
-    std::shared_ptr<Robos::NodeBase> pNetworkingClient = std::static_pointer_cast<Robos::NodeBase>(std::make_shared<Network::CommReceiver>());
-    std::shared_ptr<Robos::NodeBase> pMotorController = std::static_pointer_cast<Robos::NodeBase>(std::make_shared<MotorControl::MotorNode>());
+	// Connect to Team_02
 
-    // initialize Robos
-    Robos::Init(Utilities::OS::GetCurrentDirectory(__FILE__) +
-                Utilities::OS::GetPathSep() + "AsyncConfig.xml");
+	// Find router ip
+	std::string my_ip = std::string("192.168.8.100");
+	std::string router_ip = std::string("");
+	char buff1[100];
+	char buff2[20];
+	char buff3[20];
+	FILE *out = popen("route -n","r");
+	fgets(buff1,sizeof(buff1)-1,out);
+	fgets(buff1,sizeof(buff1)-1,out);
+	while(::fgets(buff1,sizeof(buff1)-1,out)) {
+		sscanf(buff1,"%s %s",buff2,buff3);
+		if(std::string("0.0.0.0").compare(std::string(buff2)) == 0) {
+			router_ip = std::string(buff3);
+			break;
+		}
+	}
 
-    // register Nodes with Robos
-    Robos::Register(pNetworkingClient);
-    Robos::Register(pMotorController);
+	// Find computers connected to router
+	std::string ext_ip;
+	bool found;
+	restart:
+	found = false;
+	char cmd[120];
+	strcpy(cmd, "nmap -sP ");
+	strcat(cmd,buff3);
+	strcat(cmd,"/24 | grep \"Nmap scan report for\" | awk '{print %5}'");
+	out = popen(cmd,"r");
+	while(fgets(buff1,sizeof(buff1)-1,out)) {
+		if(std::string("192.168.8.1").compare(std::string(buff1))==0) {
+			continue;
+		}
+		if(my_ip.compare(std::string(buff1))==0) {
+			continue;
+		}
+		ext_ip = std::string(buff1);
+		found = true;
+	}
+	// make a method, no time
+	if(!found) {
+		goto restart;
+	}
 
-    // Start Robos
-    Robos::Start();
+	// initialize Nodes
+	std::shared_ptr<Robos::NodeBase> pNetworkingClient = std::static_pointer_cast<Robos::NodeBase>(std::make_shared<Network::CommReceiver>());
+	std::shared_ptr<Robos::NodeBase> pMotorController = std::static_pointer_cast<Robos::NodeBase>(std::make_shared<MotorControl::MotorNode>());
 
-    // wait for timeout (or for a Node to kill)
-    std::this_thread::sleep_for(std::chrono::seconds(10));
-    // in the real version...call Robos::WaitForShutdown();
-    // this thread will go to sleep until a Node makes a call to ShutdownRobos();
-    // and then this thread will be woken up.
+	// initialize Robos
+	Robos::Init(Utilities::OS::GetCurrentDirectory(__FILE__) +
+			Utilities::OS::GetPathSep() + "AsyncConfig.xml");
 
-    // stop Robos
-    Robos::Stop();
-    return 0;
+	// register Nodes with Robos
+	Robos::Register(pNetworkingClient);
+	Robos::Register(pMotorController);
+
+	// Start Robos
+	Robos::Start();
+
+	// wait for timeout (or for a Node to kill)
+	std::this_thread::sleep_for(std::chrono::seconds(10));
+	// in the real version...call Robos::WaitForShutdown();
+	// this thread will go to sleep until a Node makes a call to ShutdownRobos();
+	// and then this thread will be woken up.
+
+	// stop Robos
+	Robos::Stop();
+	return 0;
 }
