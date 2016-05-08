e5b4a1a3aaa7cb12355c30b72d72bcac30b0b03b
Anno van den Akker <annovda@gmail.com>
2016-05-08 00:07:31 -0400

    Parsing robot controls to motor controls

--- src/MotorControl/src/MotorControl.cpp
+++ src/MotorControl/src/MotorControl.cpp
@@ -3,6 +3,7 @@
 #include "CommonMotorDataStructures.hpp"
 #include "I2C.hpp"
 #include <stdio.h>
+#include <memory>
 
 namespace MotorControl {
 
@@ -15,6 +16,82 @@ int initialize(char * device) {
 }
 
 void execute(RobotAction action) {
+	char speed = scaleVelocity(action.command,action.speed);
+	char dist = scaleDistance(action.command,action.speed);
+	MotorAction * m;
+	switch(action.command) {
+	case CMD_FWD:
+		m = (MotorAction*)::malloc(sizeof(MotorAction)*4);
+		m[0] = MotorAction(MOT_FR,speed,dist,action.ovr);
+		m[1] = MotorAction(MOT_FL,speed,dist,action.ovr);
+		m[2] = MotorAction(MOT_BR,speed,dist,action.ovr);
+		m[3] = MotorAction(MOT_BL,speed,dist,action.ovr);
+		break;
+	case CMD_BWD:
+		m = (MotorAction*)::malloc(sizeof(MotorAction)*4);
+		m[0] = MotorAction(MOT_FR,-speed,dist,action.ovr);
+		m[1] = MotorAction(MOT_FL,-speed,dist,action.ovr);
+		m[2] = MotorAction(MOT_BR,-speed,dist,action.ovr);
+		m[3] = MotorAction(MOT_BL,-speed,dist,action.ovr);
+		break;
+	case CMD_TL:
+		m = (MotorAction*)::malloc(sizeof(MotorAction)*4);
+		m[0] = MotorAction(MOT_FR,speed,dist,action.ovr);
+		m[1] = MotorAction(MOT_FL,-speed,dist,action.ovr);
+		m[2] = MotorAction(MOT_BR,speed,dist,action.ovr);
+		m[3] = MotorAction(MOT_BL,-speed,dist,action.ovr);
+		break;
+	case CMD_TR:
+		m = (MotorAction*)::malloc(sizeof(MotorAction)*4);
+		m[0] = MotorAction(MOT_FR,-speed,dist,action.ovr);
+		m[1] = MotorAction(MOT_FL,speed,dist,action.ovr);
+		m[2] = MotorAction(MOT_BR,-speed,dist,action.ovr);
+		m[3] = MotorAction(MOT_BL,speed,dist,action.ovr);
+		break;
+	case CMD_OW:
+		m = (MotorAction*)::malloc(sizeof(MotorAction)*2);
+		m[0] = MotorAction(ACT_WHEL,speed,dist,action.ovr);
+		m[1] = MotorAction(ACT_WHER,speed,dist,action.ovr);
+		break;
+	case CMD_CW:
+		m = (MotorAction*)::malloc(sizeof(MotorAction)*2);
+		m[0] = MotorAction(ACT_WHEL,-speed,dist,action.ovr);
+		m[1] = MotorAction(ACT_WHER,-speed,dist,action.ovr);
+		break;
+	case CMD_INDV:
+		break;
+	case CMD_TLTD:
+		m = (MotorAction*)::malloc(sizeof(MotorAction)*2);
+		m[0] = MotorAction(ACT_ARML,speed,dist,action.ovr);
+		m[1] = MotorAction(ACT_ARMR,speed,dist,action.ovr);
+		break;
+	case CMD_TLTU:
+		m = (MotorAction*)::malloc(sizeof(MotorAction)*2);
+		m[0] = MotorAction(ACT_ARML,-speed,dist,action.ovr);
+		m[1] = MotorAction(ACT_ARMR,-speed,dist,action.ovr);
+		break;
+	case CMD_TRAD:
+		m = (MotorAction*)::malloc(sizeof(MotorAction)*2);
+		m[0] = MotorAction(MOT_TRAL,speed,dist,action.ovr);
+		m[1] = MotorAction(MOT_TRAL,speed,dist,action.ovr);
+		break;
+	case CMD_TRAU:
+		m = (MotorAction*)::malloc(sizeof(MotorAction)*2);
+		m[0] = MotorAction(MOT_TRAL,-speed,dist,action.ovr);
+		m[1] = MotorAction(MOT_TRAL,-speed,dist,action.ovr);
+		break;
+	case CMD_BF:
+		m = (MotorAction*)::malloc(sizeof(MotorAction));
+		m[0] = MotorAction(MOT_CBUC,speed,dist,action.ovr);
+		break;
+	case CMD_BB:
+		m = (MotorAction*)::malloc(sizeof(MotorAction));
+		m[0] = MotorAction(MOT_CBUC,-speed,dist,action.ovr);
+		break;
+	case CMD_STATUS:
+
+		break;
+	}
 	int len = 3;
 	char * buffer = (char*)malloc(3);
 	buffer[0] = scaleVelocity(action.command,action.speed);
