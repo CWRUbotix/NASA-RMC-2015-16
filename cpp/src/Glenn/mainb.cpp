96db8c877202ab071e293ac2974ca62eb9b616fb
Anno van den Akker <annovda@gmail.com>
2016-05-19 11:23:46 -0400

    All stop

--- cpp/src/Glenn/mainb.cpp
+++ cpp/src/Glenn/mainb.cpp
@@ -12,30 +12,51 @@
 #include "Network/protocol.hpp"
 #include "Control/commandReceiver.hpp"
 
-int main(int argc, char** argv) {
-	/*
-	if (argc != 2) {
-		printf("Please input an IP address.\n");
-		exit(-1);
-	}
-	 */
+#include <vector>
+#include <string>
+#include <dirent.h>
+#include <iostream>
+#include <stdio.h>
 
-	//initialize_client(5005, 100, argv[0]);
-	MotorControl::initialize("/dev/ttyACM2");
+int main(int argc, char** argv) {
+	std::vector<std::string> files = std::vector<std::string>();
+	DIR* dp = opendir("/dev/");
+	struct dirent* dirp;
+	std::string file;
+	while((dirp = readdir(dp)) != NULL) {
+		file = std::string(dirp->d_name);
+		if(!file.substr(0,6).compare("ttyACM")) {
+			files.push_back(std::string(dirp->d_name));
+		}
+	}
+	closedir(dp);
+
+	if(!files.size()) {
+		printf("Arduino is not connected");
+		return -1;
+	}
+	std::string file = "/dev/" + files.front();
+	char* c = file.c_str();
+	MotorControl::initialize(c);
 	sleep(1);
 	printf("Start listening.\n");
 	char command[MAX_RECV_LEN];
 
 	while(1) {
-		//get_command(command);
-		//printf("Got command\n");
-		//send_reply ((char *)"msg rcvd\n", strlen ("msg rcvd\n") + 1);
-
-		printf("Please enter a v command.\n");
+		printf("Please enter a command.\n");
 		scanf("%s", command);
 
-
-		if(command[0] == 'w') {
+		if(command[0] == 'o') {
+			uint8_t m[4] = {MOT_FR,MOT_FL,MOT_BR,MOT_BL,MOT_TRAL,MOT_TRAR,MOT_CBUC,MOT_CHOP,ACT_WHEL,ACT_WHER,ACT_ARML,ACT_ARML};
+			bool d[12];
+			MotorControl::ActionSimple action(m,d,12,0);
+			MotorControl::queueAction(action);
+		} else if(command[0] == 'p') {
+			uint8_t m[4] = {MOT_FR,MOT_FL,MOT_BR,MOT_BL};
+			bool d[4] = {false,true,false,true};
+			MotorControl::ActionSimple action(m,d,4,0);
+			MotorControl::queueAction(action);
+		} else if(command[0] == 'w') {
 			uint8_t m[4] = {MOT_FR,MOT_FL,MOT_BR,MOT_BL};
 			bool d[4] = {false,true,false,true};
 			MotorControl::ActionSimple action(m,d,4,100);
@@ -91,108 +112,6 @@ int main(int argc, char** argv) {
 			MotorControl::ActionSimple action(m,d,1,0);
 			MotorControl::queueAction(action);
 		}
-/*
-		if(command[0] == 'p') {
-			MotorControl::port.close();
-			printf("Closing port\n");
-			sleep(1);
-			MotorControl::port.open();
-			sleep(1);
-			printf("Opening port\n");
-		} else if(command[0] == 'z') {
-			uint8_t m[5] = {MOT_FR,MOT_FL,MOT_BR,MOT_BL,MOT_CHOP};
-			bool d[5] = {false,true,false,true,true};
-			MotorControl::ActionSimple action(m,d,5,0);
-			MotorControl::queueAction(action);
-		}else if(command[0] == 'w') {
-			uint8_t m[4] = {MOT_FR,MOT_FL,MOT_BR,MOT_BL};
-			bool d[4] = {false,true,false,true};
-			MotorControl::ActionSimple action(m,d,4,125);
-			MotorControl::queueAction(action);
-		} else if(command[0] == 's') {
-			uint8_t m[4] = {MOT_FR,MOT_FL,MOT_BR,MOT_BL};
-			bool d[4] = {true,false,true,false};
-			MotorControl::ActionSimple action(m,d,4,125);
-			MotorControl::queueAction(action);
-		} else if(command[0] == 'a') {
-			uint8_t m[4] = {MOT_FR,MOT_FL,MOT_BR,MOT_BL};
-			bool d[4] = {false,false,false,false};
-			MotorControl::ActionSimple action(m,d,4,125);
-			MotorControl::queueAction(action);
-		} else if(command[0] == 'd') {
-			uint8_t m[4] = {MOT_FR,MOT_FL,MOT_BR,MOT_BL};
-			bool d[4] = {true,true,true,true};
-			MotorControl::ActionSimple action(m,d,4,125);
-			MotorControl::queueAction(action);
-		} else if(command[0] == 'q') {
-			uint8_t m[2] = {ACT_WHEL,ACT_WHER};
-			bool d[4] = {true,true};
-			MotorControl::ActionSimple action(m,d,2,125);
-			MotorControl::queueAction(action);
-			std::this_thread::sleep_for(std::chrono::milliseconds(200));
-			MotorControl::ActionSimple action1(m,d,2,0);
-			MotorControl::queueAction(action1);
-		} else if(command[0] == 'e') {
-			uint8_t m[2] = {ACT_WHEL,ACT_WHER};
-			bool d[4] = {false,false};
-			MotorControl::ActionSimple action(m,d,2,125);
-			MotorControl::queueAction(action);
-			std::this_thread::sleep_for(std::chrono::milliseconds(200));
-			MotorControl::ActionSimple action1(m,d,2,0);
-			MotorControl::queueAction(action1);
-		} else if(command[0] == 'h') {
-			uint8_t m[1] = {MOT_CHOP};
-			bool d[4] = {false};
-			MotorControl::ActionSimple action(m,d,1,40);
-			MotorControl::queueAction(action);
-		} else if (command[0] == 'y') {
-			uint8_t m[1] = {MOT_CHOP};
-			bool d[4] = {false};
-			MotorControl::ActionSimple action(m,d,1,0);
-			MotorControl::queueAction(action);
-		} else if(command[0] == 'b') {
-			uint8_t m[1] = {MOT_CBUC};
-			bool d[4] = {true};
-			MotorControl::ActionSimple action(m,d,1,100);
-			MotorControl::queueAction(action);
-			std::this_thread::sleep_for(std::chrono::milliseconds(400));
-			MotorControl::ActionSimple action1(m,d,1,0);
-			MotorControl::queueAction(action1);
-		} else if(command[0] == 't') {
-			// Left true is up
-			uint8_t m[2] = {MOT_TRAL,MOT_TRAR};
-			bool d[4] = {true,true};
-			MotorControl::ActionSimple action(m,d,2,80);
-			MotorControl::queueAction(action);
-			std::this_thread::sleep_for(std::chrono::milliseconds(500));
-			MotorControl::ActionSimple action1(m,d,2,0);
-			MotorControl::queueAction(action1);
-		}  else if(command[0] == 'r') {
-			// Left true is up
-			uint8_t m[2] = {MOT_TRAL,MOT_TRAR};
-			bool d[4] = {false,false};
-			MotorControl::ActionSimple action(m,d,2,80);
-			MotorControl::queueAction(action);
-			std::this_thread::sleep_for(std::chrono::milliseconds(400));
-			MotorControl::ActionSimple action1(m,d,2,0);
-			MotorControl::queueAction(action1);
-		} else if(command[0] == 'f') {
-			uint8_t m[2] = {MOT_TRAL};
-			bool d[4] = {true,true};
-			MotorControl::ActionSimple action(m,d,1,40);
-			MotorControl::queueAction(action);
-			std::this_thread::sleep_for(std::chrono::milliseconds(100));
-			MotorControl::ActionSimple action1(m,d,1,0);
-			MotorControl::queueAction(action1);
-		}  else if(command[0] == 'g') {
-			uint8_t m[2] = {MOT_TRAL,ACT_ARMR};
-			bool d[4] = {false,false};
-			MotorControl::ActionSimple action(m,d,1,40);
-			MotorControl::queueAction(action);
-			std::this_thread::sleep_for(std::chrono::milliseconds(100));
-			MotorControl::ActionSimple action1(m,d,1,0);
-			MotorControl::queueAction(action1);
-		}*/
 	}
 	return 0;
 }
