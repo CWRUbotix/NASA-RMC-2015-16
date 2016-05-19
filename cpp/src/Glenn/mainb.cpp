299eb55fc19ab7681bac9b17d0ea9efe8295256e
Paul Nettleton <paulnett7@hotmail.com>
2016-05-19 15:36:23 -0400

    Fixed build errors.

--- cpp/src/Glenn/mainb.cpp
+++ cpp/src/Glenn/mainb.cpp
@@ -17,7 +17,6 @@
 #include <dirent.h>
 #include <iostream>
 #include <stdio.h>
-#include <glib.h>
 
 int main(int argc, char** argv) {
 	if (argc != 2)
@@ -25,7 +24,7 @@ int main(int argc, char** argv) {
 		printf("Please enter the IP address of the control station.");
 		exit(-1);
 	}
-
+	/*
 	std::vector<std::string> files = std::vector<std::string>();
 	DIR* dp = opendir("/dev/");
 	struct dirent* dirp;
@@ -42,9 +41,9 @@ int main(int argc, char** argv) {
 		printf("Arduino is not connected");
 		return -1;
 	}
-	std::string file = "/dev/" + files.front();
-	char* c = file.c_str();
-	MotorControl::initialize(c);
+	std::string filep = "/dev/" + files.front();
+	const char* c = filep.c_str();*/
+	MotorControl::initialize("/dev/ttyACM0");
 	sleep(1);
 
 	initialize_client(5005, 100, argv[0]);
@@ -57,14 +56,33 @@ int main(int argc, char** argv) {
 		//scanf("%s", command);	
 
 		get_command(rcvd_command);
+		printf(rcvd_command);
 		send_reply ((char *)"msg rcvd\n", strlen("msg rcvd\n") + 1);
+		
+		std::string command_string = std::string(rcvd_command);
+		std::string delimiter = " ";
+		std::string commands[2];
 
-		char** commands = g_strsplit(rcvd_command, ' ', 0);
-		char* command = commands[0];
-		int speed = atoi(commands[1]);
+		size_t pos = 0;
+		pos = command_string.find(delimiter);
+		commands[0] = command_string.substr(0, pos);
+		command_string.erase(0, pos + delimiter.length());
+		pos = command_string.find(delimiter);
+		commands[1] = command_string.substr(0, pos);
+		command_string.erase(0, pos + delimiter.length());
+
+		const char* command = commands[0].c_str();
+
+		int speed = 0;
+		if (!commands[1].empty()) 
+			speed = atoi(commands[1].c_str());
+		if (speed < 0)
+			speed = 0;
+		else if (speed > 125)
+			speed = 125;
 
 		if(command[0] == 'o') {
-			uint8_t m[4] = {MOT_FR,MOT_FL,MOT_BR,MOT_BL,MOT_TRAL,MOT_TRAR,MOT_CBUC,MOT_CHOP,ACT_WHEL,ACT_WHER,ACT_ARML,ACT_ARML};
+			uint8_t m[12] = {MOT_FR,MOT_FL,MOT_BR,MOT_BL,MOT_TRAL,MOT_TRAR,MOT_CBUC,MOT_CHOP,ACT_WHEL,ACT_WHER,ACT_ARML,ACT_ARML};
 			bool d[12];
 			MotorControl::ActionSimple action(m,d,12,0);
 			MotorControl::queueAction(action);
@@ -76,32 +94,44 @@ int main(int argc, char** argv) {
 		} else if(command[0] == 'w') {
 			uint8_t m[4] = {MOT_FR,MOT_FL,MOT_BR,MOT_BL};
 			bool d[4] = {false,true,false,true};
-			MotorControl::ActionSimple action(m,d,4,100);
+			if (commands[1].empty())
+				speed = 100;
+			MotorControl::ActionSimple action(m,d,4,speed);
 			MotorControl::queueAction(action);
 		} else if(command[0] == 's') {
 			uint8_t m[4] = {MOT_FR,MOT_FL,MOT_BR,MOT_BL};
 			bool d[4] = {true,false,true,false};
-			MotorControl::ActionSimple action(m,d,4,100);
+			if (commands[1].empty())
+				speed = 100;
+			MotorControl::ActionSimple action(m,d,4,speed);
 			MotorControl::queueAction(action);
 		} else if(command[0] == 'd') {
 			uint8_t m[4] = {MOT_FR,MOT_FL,MOT_BR,MOT_BL};
 			bool d[4] = {true,true,true,true};
-			MotorControl::ActionSimple action(m,d,4,100);
+			if (commands[1].empty())
+				speed = 100;
+			MotorControl::ActionSimple action(m,d,4,speed);
 			MotorControl::queueAction(action);
 		} else if(command[0] == 'a') {
 			uint8_t m[4] = {MOT_FR,MOT_FL,MOT_BR,MOT_BL};
 			bool d[4] = {false,false,false,false};
-			MotorControl::ActionSimple action(m,d,4,100);
+			if (commands[1].empty())
+				speed = 100;
+			MotorControl::ActionSimple action(m,d,4,speed);
 			MotorControl::queueAction(action);
 		} else if(command[0] == 'e') {
 			uint8_t m[2] = {MOT_TRAL,MOT_TRAR};
 			bool d[4] = {true,true};
-			MotorControl::ActionSimple action(m,d,2,40);
+			if (commands[1].empty())
+				speed = 40;
+			MotorControl::ActionSimple action(m,d,2,speed);
 			MotorControl::queueAction(action);
 		} else if(command[0] == 'r') {
 			uint8_t m[2] = {MOT_TRAL,MOT_TRAR};
 			bool d[4] = {false,false};
-			MotorControl::ActionSimple action(m,d,2,40);
+			if (commands[1].empty())
+				speed = 40;
+			MotorControl::ActionSimple action(m,d,2,speed);
 			MotorControl::queueAction(action);
 		} else if(command[0] == 'q') {
 			uint8_t m[2] = {MOT_TRAL,MOT_TRAR};
@@ -111,12 +141,16 @@ int main(int argc, char** argv) {
 		} else if(command[0] == 'x') {
 			uint8_t m[1] = {MOT_CBUC};
 			bool d[4] = {true};
-			MotorControl::ActionSimple action(m,d,1,100);
+			if (commands[1].empty())
+				speed = 100;
+			MotorControl::ActionSimple action(m,d,1,speed);
 			MotorControl::queueAction(action);
 		} else if(command[0] == 'v') {
 			uint8_t m[1] = {MOT_CHOP};
 			bool d[4] = {false};
-			MotorControl::ActionSimple action(m,d,1,40);
+			if (commands[1].empty())
+				speed = 40;
+			MotorControl::ActionSimple action(m,d,1,speed);
 			MotorControl::queueAction(action);
 		} else if(command[0] == 'z') {
 			uint8_t m[1] = {MOT_CBUC};
