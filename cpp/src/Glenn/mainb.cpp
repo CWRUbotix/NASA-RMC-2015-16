13f8f92c4316066dfdfabab2a4a12a30be3ea979
Paul Nettleton <paulnett7@hotmail.com>
2016-05-19 19:43:47 -0400

    Made it so that it is possible to not input a speed

--- cpp/src/Glenn/mainb.cpp
+++ cpp/src/Glenn/mainb.cpp
@@ -54,36 +54,44 @@ int main(int argc, char** argv) {
 	while(1) {
 		printf("Please enter a command.\n");
 
+		std::string rcvd_command;
+
+		std::getline(std::cin, rcvd_command);
+
+		/*
 		char command[2];
 		char sp_ptr[3];
 		scanf("%s %s", command, sp_ptr);
 		char c;
 		while((c = getchar()) != '\n' && c != EOF);
+		*/
 		
 		//get_command(rcvd_command);
 		//printf("%s\n", rcvd_command);
 		//send_reply ((char *)"msg rcvd\n", strlen("msg rcvd\n") + 1);
 
-		/*
+		std::string command_string;
+		std::string speed_command;
 		size_t pos = 0;
-		pos = command_string.find(delimiter);
-		commands[0] = command_string.substr(0, pos);
-		command_string.erase(0, pos + delimiter.length());
-		pos = command_string.find(delimiter);
-		commands[1] = command_string.substr(0, pos);
-		command_string.erase(0, pos + delimiter.length());
-		*/
+		std::string delimiter = " ";
+		pos = rcvd_command.find(delimiter);
+		command_string = rcvd_command.substr(0, pos);
+		rcvd_command.erase(0, pos + delimiter.length());
+		pos = rcvd_command.find(delimiter);
+		speed_command = rcvd_command.substr(0, pos);
+		rcvd_command.erase(0, pos + delimiter.length());
 
 		//const char* command = commands[0].c_str();
 
 		int speed = 0;
-		const char speed_command[3] = { sp_ptr[0], sp_ptr[1], sp_ptr[2]};
-		if (sp_ptr != '\0') 
-			speed = atoi(speed_command);
+		if (!speed_command.empty()) 
+			speed = atoi(speed_command.c_str());
 		if (speed < 0)
 			speed = 0;
 		else if (speed > 125)
 			speed = 125;
+
+		const char* command = command_string.c_str();
 
 		if(command[0] == 'o') {
 			uint8_t m[12] = {MOT_FR,MOT_FL,MOT_BR,MOT_BL,MOT_TRAL,MOT_TRAR,MOT_CBUC,MOT_CHOP,ACT_WHEL,ACT_WHER,ACT_ARML,ACT_ARML};
@@ -98,42 +106,42 @@ int main(int argc, char** argv) {
 		} else if(command[0] == 'w') {
 			uint8_t m[4] = {MOT_FR,MOT_FL,MOT_BR,MOT_BL};
 			bool d[4] = {false,true,false,true};
-			if (command [1] != '\0')
+			if (speed_command.empty())
 				speed = 100;
 			MotorControl::ActionSimple action(m,d,4,speed);
 			MotorControl::queueAction(action);
 		} else if(command[0] == 's') {
 			uint8_t m[4] = {MOT_FR,MOT_FL,MOT_BR,MOT_BL};
 			bool d[4] = {true,false,true,false};
-			if (command [1] != '\0')
+			if (speed_command.empty())
 				speed = 100;
 			MotorControl::ActionSimple action(m,d,4,speed);
 			MotorControl::queueAction(action);
 		} else if(command[0] == 'd') {
 			uint8_t m[4] = {MOT_FR,MOT_FL,MOT_BR,MOT_BL};
 			bool d[4] = {true,true,true,true};
-			if (command [1] != '\0')
+			if (speed_command.empty())
 				speed = 100;
 			MotorControl::ActionSimple action(m,d,4,speed);
 			MotorControl::queueAction(action);
 		} else if(command[0] == 'a') {
 			uint8_t m[4] = {MOT_FR,MOT_FL,MOT_BR,MOT_BL};
 			bool d[4] = {false,false,false,false};
-			if (command [1] != '\0')
+			if (speed_command.empty())
 				speed = 100;
 			MotorControl::ActionSimple action(m,d,4,speed);
 			MotorControl::queueAction(action);
 		} else if(command[0] == 'e') {
 			uint8_t m[2] = {MOT_TRAL,MOT_TRAR};
 			bool d[4] = {true,true};
-			if (command [1] != '\0')
+			if (speed_command.empty())
 				speed = 40;
 			MotorControl::ActionSimple action(m,d,2,speed);
 			MotorControl::queueAction(action);
 		} else if(command[0] == 'r') {
 			uint8_t m[2] = {MOT_TRAL,MOT_TRAR};
 			bool d[4] = {false,false};
-			if (command [1] != '\0')
+			if (speed_command.empty())
 				speed = 40;
 			MotorControl::ActionSimple action(m,d,2,speed);
 			MotorControl::queueAction(action);
@@ -145,14 +153,14 @@ int main(int argc, char** argv) {
 		} else if(command[0] == 'x') {
 			uint8_t m[1] = {MOT_CBUC};
 			bool d[4] = {true};
-			if (command [1] != '\0')
+			if (speed_command.empty())
 				speed = 100;
 			MotorControl::ActionSimple action(m,d,1,speed);
 			MotorControl::queueAction(action);
 		} else if(command[0] == 'v') {
 			uint8_t m[1] = {MOT_CHOP};
 			bool d[4] = {false};
-			if (command [1] != '\0')
+			if (speed_command.empty())
 				speed = 40;
 			MotorControl::ActionSimple action(m,d,1,speed);
 			MotorControl::queueAction(action);
